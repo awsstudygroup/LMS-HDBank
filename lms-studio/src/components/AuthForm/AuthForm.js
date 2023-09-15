f1e134e7e67d2178c3dbec306fa6a6e9648e950a
PhamTHHanh <103091745+PhamTHHanh@users.noreply.github.com>
2023-09-15 13:06:11 +0700

    Fix update course miss attribute

--- lms-studio/src/components/AuthForm/AuthForm.js
+++ lms-studio/src/components/AuthForm/AuthForm.js
@@ -1,11 +1,59 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator,
+  Theme,
+  ThemeProvider,
+  useTheme
+ } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Navigate } from 'react-router-dom';
+import './AuthForm.css';
 
 export default function AuthForm() {
+  const { tokens } = useTheme();
+  console.log(tokens);
+  const theme: Theme = {
+    name: 'Auth Example Theme',
+    tokens: {
+      colors: {
+        brand: {
+          primary: {
+            '10': tokens.colors.teal['100'],
+            '80': tokens.colors.teal['40'],
+            '90': tokens.colors.teal['20'],
+            '100': tokens.colors.teal['10'],
+          },
+        },
+      },
+      components: {
+        tabs: {
+          item: {
+            _focus: {
+              color: {
+                value: '#EC7211',
+              }
+            },
+            _hover: {
+              color: {
+                value: '#ffa963',
+              },
+            },
+            _active: {
+              color: {
+                value: '#EC7211',
+              },
+              borderColor: {
+                value: '#EC7211'
+              }
+            },
+          },
+        },
+      },
+    },
+  };
   return (
+    <ThemeProvider theme={theme}>
     <Authenticator>
       <Navigate to="/" replace={true} />
     </Authenticator>
+    </ThemeProvider>
   );
 }
